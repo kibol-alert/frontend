16dff2c3d27a5f8234dd09bca5f7e282d42de01c
Przemysław Jaśkiewicz <przemo95732@gmail.com>
2019-11-12 18:19:45 +0100

    Registration

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SignInSide from '../layouts/LandingPage.js';
+import '../styles/App.css';
+
+function App() {
+  return (
+
+    
+    <div >
+ <SignInSide/>
+    </div>
+  );
+}
+
+export default App;
