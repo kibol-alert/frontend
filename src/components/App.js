1eb908f5bc27de5bb90ff8244eb7cffc44fda3a9
Przemysław Jaśkiewicz <przemo95732@gmail.com>
2019-11-12 18:19:45 +0100

    Registration

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Registration from './Registration.js'
+
+import '../styles/App.css';
+
+function App() {
+  return (
+
+    
+    <div >
+ <Registration/>
+    </div>
+  );
+}
+
+export default App;
