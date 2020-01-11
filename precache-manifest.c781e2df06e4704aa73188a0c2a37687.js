a7e9664b75841fba289cf9553558ecfe4232352c
bboysokol <bboy_sokol@o2.pl>
2020-01-12 00:49:07 +0100

    Updates

---
+++ precache-manifest.c781e2df06e4704aa73188a0c2a37687.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f28a13aae35bf099cb3713c944ef572",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "b1a90443673ce2991d12",
+    "url": "/frontend/static/css/2.93b8273e.chunk.css"
+  },
+  {
+    "revision": "5f5347e57794e5d3de90",
+    "url": "/frontend/static/css/main.f1d422bc.chunk.css"
+  },
+  {
+    "revision": "b1a90443673ce2991d12",
+    "url": "/frontend/static/js/2.63263796.chunk.js"
+  },
+  {
+    "revision": "bfd5c02bdce068aa3e0fba43f53d7547",
+    "url": "/frontend/static/js/2.63263796.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f5347e57794e5d3de90",
+    "url": "/frontend/static/js/main.175ade4f.chunk.js"
+  },
+  {
+    "revision": "515fc8e952bcc74e9b6e",
+    "url": "/frontend/static/js/runtime-main.c812f4f8.js"
+  },
+  {
+    "revision": "a66d32af7c4618f91f1220f7f2f190a6",
+    "url": "/frontend/static/media/logo-back.a66d32af.png"
+  }
+]);
